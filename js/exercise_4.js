a25968b7a9a11ab3c8913e0b3b15663c896c1395
Scott Wolter <mail@ScottWolter.com>
2016-05-02 15:23:04 -0400

    Added code to bind popup text to name property

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -36,6 +36,12 @@ featureLayer.on('ready', function(){
 })
 
 
+featureLayer.on('ready', function(){
+	this.eachLayer(function(layer){
+    	layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+    })
+})
+
 
 
 
