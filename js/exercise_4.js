bedd5a812a54664ef8c854e83a90a87b0d511d7c
Scott Wolter <mail@ScottWolter.com>
2016-05-02 15:55:19 -0400

    fixed js typo

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -48,28 +48,28 @@ var clickHandler = function(e){
   
   var feature = e.target.feature;
   
-  $('#sidebar').fadein(400, function(){
-  	var info = '';
-    
-    info += '<div>';
-    info += '<h2>' + feature.properties.name + '</h2>';
-    if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '</p>';
-    if(feature.properties.phone) info += '<p>' + feature.properties.phone + '</p>';
-    if(feature.properties.website) info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a></p>';
-    
-    info += '</div>';
-    
-    $('#info').append(info)
-  })
-//}
+  $('#info').fadeIn(400, function(){
+    var info = '';
+
+    info += '<div>'
+    info += '<h2>' + feature.properties.name + '</h2>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.cuisine + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.phone + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.website + '</p>'
+    if(feature.properties.phone) info +=   '<p><a href="' + feature.properties.website + '">'  + feature.properties.website + '</a></p>'
+    info += '</div>'
+
+    $('#info').append(info);
+  });
+};
 
 featureLayer.on('ready', function(){
-	this.eachLayer(function(layer){
-    	layer.on('click', clickHandler);
-    })
-})
+  this.eachLayer(function(layer){
+    layer.on('click', clickHandler);
+  });
+});
 
-map.on('click', function(){
-	$('#sidebar').fadeout(200);
-})
-}
+map.on('click',function(e){
+    $('#info').fadeOut(200);
+    $('#info').empty();
+});
